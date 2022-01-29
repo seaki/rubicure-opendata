edd13b599fdd5a51c53b839b2e2b746d99b2b979
Se AKi <seaki@sastudio.jp>
2022-01-30 07:38:16 +0900

    Add spec about POST

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "rubicure-opendata Application" do
@@ -6,8 +7,13 @@ describe "rubicure-opendata Application" do
     expect(last_response).to be_ok
   end
 
-  it "should allow accessing the sparql endpoint" do
+  it "should allow accessing the sparql endpoint with GET" do
     get '/sparql'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow accessing the sparql endpoint with POST" do
+    post '/sparql'
     expect(last_response).to be_ok
   end
 end
